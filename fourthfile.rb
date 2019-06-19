d0f0250e9e3a9cc2c2c694ea193462854c6966d6
yokesh440 <39620639+yokesh440@users.noreply.github.com>
2019-06-19 18:17:46 +0530

    Add files via upload

---
+++ fourthfile.rb
@@ -0,0 +1,18 @@
+a = 10
+b = a +
+        10
+c = [ 5, 4, 
+        10 ]
+d = [ a ] \
+        + c
+print "#{a} #{b} [", c.join(" "), "] [", d.join(" "), "]\n";
+# Simple for loop using a range.
+for i in (1..4)
+    print i," "
+end
+print "\n"
+
+for i in (1...4)
+    print i," "
+end
+print "\n"
