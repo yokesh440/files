68c2f98742f0811b9cbdc6b237a360db4fa3bcec
yokesh440 <39620639+yokesh440@users.noreply.github.com>
2019-06-21 01:39:44 +0530

    Update fourthfile.rb

--- fourthfile.rb
+++ fourthfile.rb
@@ -1,3 +1,21 @@
+module FourKitesParsers::Helper::OtmHelpers::ShipmentStopElementsHelper
+  include FourKitesParsers::Helper::OtmHelpers::ShipmentStopElementsAdditionalHelper
+        
+    def parse_shipment_appointment_time(shipment_related_items, stop, parsed_shipment_stop)
+    shipment_stop_attr_element = shipment_related_items[:shipment_stop_attr_element]
+    case shipment_stop_attr_element.name
+    when 'Appointment'
+      parse_shipment_stop_appointment(stop, shipment_stop_attr_element)
+    when 'ArrivalTime'
+      parse_arrrival_time(shipment_stop_attr_element, stop)
+    when 'AppointmentPickup', 'AppointmentDelivery'
+      g_log_date = shipment_stop_attr_element.children.detect { |element| element.name == 'GLogDate'}
+      stop[:arrival_time] =parse_time(g_log_date.children.first.text) if g_log_date.children.first.present? && g_log_date.children.first.text.present?
+    end
+  end
+        
+end
+
 a = 10
 b = a +
         10
@@ -16,3 +34,4 @@ for i in (1...4)
     print i," "
 end
 print "\n"
+
